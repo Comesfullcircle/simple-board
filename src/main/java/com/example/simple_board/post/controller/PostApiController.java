69c7f70a05538c1245f0a6206071e4c320823e20
윤설이 <olerlmin1234@gmail.com>
2024-09-24 12:06:15 +0900

    전체 게시글 보기, 게시글 열람, 게시글 삭제 create

--- src/main/java/com/example/simple_board/post/controller/PostApiController.java
+++ src/main/java/com/example/simple_board/post/controller/PostApiController.java
@@ -2,13 +2,13 @@ package com.example.simple_board.post.controller;
 
 import com.example.simple_board.post.db.PostEntity;
 import com.example.simple_board.post.model.PostRequest;
+import com.example.simple_board.post.model.PostViewRequest;
 import com.example.simple_board.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
@@ -24,5 +24,29 @@ public class PostApiController {
             PostRequest postRequest
     ){
         return postService.create(postRequest);
+    }
+
+    @PostMapping("/view") //비회원 게시판이므로 id받아와서 getmapping이 아니라 postmapping 으로 받아오는 점 참고
+    public PostEntity view(
+            @Valid
+            @RequestBody
+            PostViewRequest postViewRequest
+    ){
+        return postService.view(postViewRequest);
+
+    }
+
+    @GetMapping("/all")
+    public List<PostEntity> list(){
+        return postService.all();
+    }
+
+    @PostMapping("/delete")
+    public void delete(
+            @Valid
+            @RequestBody
+            PostViewRequest postViewRequest
+    ){
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/simple_board/post/db/PostRepository.java
+++ src/main/java/com/example/simple_board/post/db/PostRepository.java
@@ -3,5 +3,10 @@ package com.example.simple_board.post.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    //select * from post where id = ? and status = ? order by id desc limit 1
+    Optional<PostEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, String status);
 }
