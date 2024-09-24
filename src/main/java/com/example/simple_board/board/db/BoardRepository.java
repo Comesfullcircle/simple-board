9d4c7952a70a058d49e365bda44a31a30f61cdf5
윤설이 <olerlmin1234@gmail.com>
2024-09-24 11:09:46 +0900

    게시판 생성, 게시글 작성 create

--- src/main/java/com/example/simple_board/board/db/BoardRepository.java
+++ src/main/java/com/example/simple_board/board/db/BoardRepository.java
@@ -1,6 +1,7 @@
 package com.example.simple_board.board.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 }
