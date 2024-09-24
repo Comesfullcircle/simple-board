966e1f4ef21a8893d49075639e6766cc2b7c0dae
윤설이 <olerlmin1234@gmail.com>
2024-09-24 12:40:03 +0900

    답변 작성 create

--- src/main/java/com/example/simple_board/post/db/PostEntity.java
+++ src/main/java/com/example/simple_board/post/db/PostEntity.java
@@ -1,10 +1,13 @@
 package com.example.simple_board.post.db;
 
+import com.example.simple_board.reply.db.ReplyEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Getter
 @Setter
@@ -27,5 +30,9 @@ public class PostEntity {
     @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime postedAt;
+
+    //답변이 없을수도 있으니 답변이 안달리는 것을 default 설정
+    @Transient //데이터베이스의 컬럼으로 인식되지 않도록 @Transient 어노테이션 추가
+    private List<ReplyEntity> replyList = Arrays.asList();
 
 }
